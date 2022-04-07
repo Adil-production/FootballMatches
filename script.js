f787b5a4dab9ea961a3d315e4d7790ad4919a92e
Porbet <72229076+Adil-production@users.noreply.github.com>
2022-04-07 15:32:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,15 +11,6 @@ async function get(){
     showDatas(data.response)
     console.log(data)
 }
-
-
-
-
-
-
-
-
-
 function showDatas(datas) {
     document.getElementById("numberOfMatch").innerHTML = datas.length;
 
@@ -49,7 +40,7 @@ function showDatas(datas) {
               <br>
               <br>
               
-              <a href="${matchviewUrl}"><button class="btn btn-primary">Watch match</button></a>
+              <a href="${matchviewUrl}"><button class="btn btn-warning">Watch match</button></a>
             </div>
           </div>
         </div>
@@ -63,134 +54,3 @@ function showDatas(datas) {
     })
 }
 
-function settingOut(){
-
-        document.getElementById("setting").style.display = "block";
-        document.getElementById("setting-out").style.display = "none";
-        document.getElementById("home").style.display = "none";
-        document.getElementById("home-out").style.display = "block";
-        document.getElementById("main").style.display = "none";
-        document.getElementById("top").style.display = "none";
-        document.getElementById("up").style.display = "none";
-        document.getElementById("setting-frame").style.display = "block";
-        
-    }
-
-function homeOut(){
-
-        document.getElementById("home").style.display = "block";
-        document.getElementById("home-out").style.display = "none";
-        document.getElementById("setting").style.display = "none";
-        document.getElementById("setting-out").style.display = "block";
-        document.getElementById("main").style.display = "block";
-        document.getElementById("top").style.display = "block";
-        document.getElementById("up").style.display = "block";
-        document.getElementById("setting-frame").style.display = "none";
-    }
-
-
-
-    function myFunction() {
-        var checkBox = document.getElementById("myCheck");
-        localStorage.setItem("theme" , "dark")
-        if (checkBox.checked == true){
-            //navbar
-            document.getElementById("nav").classList.remove("navbar-light")
-            document.getElementById("nav").classList.remove("bg-light")
-            document.getElementById("nav").classList.add("navbar-dark")
-            document.getElementById("nav").classList.add("bg-dark")
-
-            //bottom navbar
-            document.getElementById("navBottom").classList.add("bg-dark")
-            document.getElementById("setting").classList.add("white")
-            document.getElementById("setting-out").classList.add("white")
-            document.getElementById("home").classList.add("white")
-            document.getElementById("home-out").classList.add("white")
-            //background
-            document.body.classList.remove("bg-light")
-            document.body.classList.add("bg-dark")
-            //main title
-            document.getElementById("top").classList.add("text-white")
-            //some stuff
-            document.getElementById("dark").classList.add("text-white")
-          
-        } else {
-            localStorage.setItem("theme" , "light")
-            document.getElementById("nav").classList.add("navbar-light")
-            document.getElementById("nav").classList.add("bg-light")
-            document.getElementById("nav").classList.remove("navbar-dark")
-            document.getElementById("nav").classList.remove("bg-dark")
-            //bottom navbar
-            document.getElementById("navBottom").classList.remove("bg-dark")
-            document.getElementById("setting").classList.remove("white")
-            document.getElementById("setting-out").classList.remove("white")
-            document.getElementById("home").classList.remove("white")
-            document.getElementById("home-out").classList.remove("white")
-            //background
-            document.body.classList.add("bg-light")
-            document.body.classList.remove("bg-dark")
-            //main title
-            document.getElementById("top").classList.remove("text-white")
-            //some stuff
-            document.getElementById("dark").classList.remove("text-white")
-            
-        }
-      }
-      function get1(){
-          if(localStorage.getItem("theme") == "dark"){
-              //navbar
-            document.getElementById("nav").classList.remove("navbar-light")
-            document.getElementById("nav").classList.remove("bg-light")
-            document.getElementById("nav").classList.add("navbar-dark")
-            document.getElementById("nav").classList.add("bg-dark")
-
-            //bottom navbar
-            document.getElementById("navBottom").classList.add("bg-dark")
-            document.getElementById("setting").classList.add("white")
-            document.getElementById("setting-out").classList.add("white")
-            document.getElementById("home").classList.add("white")
-            document.getElementById("home-out").classList.add("white")
-            //background
-            document.body.classList.remove("bg-light")
-            document.body.classList.add("bg-dark")
-            //main title
-            document.getElementById("top").classList.add("text-white")
-            //some stuff
-            document.getElementById("dark").classList.add("text-white")
-          }
-          else if(localStorage.getItem("theme") == "light"){
-            localStorage.setItem("theme" , "light")
-            document.getElementById("nav").classList.add("navbar-light")
-            document.getElementById("nav").classList.add("bg-light")
-            document.getElementById("nav").classList.remove("navbar-dark")
-            document.getElementById("nav").classList.remove("bg-dark")
-            //bottom navbar
-            document.getElementById("navBottom").classList.remove("bg-dark")
-            document.getElementById("setting").classList.remove("white")
-            document.getElementById("setting-out").classList.remove("white")
-            document.getElementById("home").classList.remove("white")
-            document.getElementById("home-out").classList.remove("white")
-            //background
-            document.body.classList.add("bg-light")
-            document.body.classList.remove("bg-dark")
-            //main title
-            document.getElementById("top").classList.remove("text-white")
-            //some stuff
-            document.getElementById("dark").classList.remove("text-white")
-          }
-          if(localStorage.getItem("theme") == "light" && document.getElementById("myCheck").checked == true){
-            document.getElementById("myCheck").click()
-          }else{
-
-          }
-          if(localStorage.getItem("theme") == "dark" && document.getElementById("myCheck").checked == false){
-            document.getElementById("myCheck").click()
-          }else{
-
-          } 
-          if(localStorage.getItem("theme") == "" || localStorage.getItem("theme") == null){
-              localStorage.setItem("theme" , "light")
-          }else{
-
-          }
-      }
